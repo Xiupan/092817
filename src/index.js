78e042a686ff76576170050ba1133ea4a7d553fa
Alan Hong <xiupan@gmail.com>
2017-09-28 10:32:25 -0500

    initial commit.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import './styles/App.css';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter} from 'react-router-dom'
+// IMPORT REACT ROUTER <<<<<<<<<<<<<<<<<<<<<<
+
+// IMPORT COMPONENTS <<<<<<<<<<<<<<<<<<<<<<
+
+
+ReactDOM.render(
+
+        {/* >>>>>>>>>>>>>>>>>>>>>
+          Add a router components with routes to:
+          Explorer. Use an exact path to "/"
+          Starships
+          Films
+          People
+          <<<<<<<<<<<<<<<<<<<<<<
+          */}
+
+
+,document.getElementById('root'));
+registerServiceWorker();
